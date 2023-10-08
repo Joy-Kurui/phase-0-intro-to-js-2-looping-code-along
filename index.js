60a95cbc1defb0e4492feaa601bc1339d475594f
joy Kurui <kurui.chepngetich@gmail.com>
2023-10-08 22:26:30 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const persons = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(persons, occasion ){
+    const newMessage = [];
+    for(let i = 0; i < persons.length; i++){
+        const message = `Thank you, ${persons[i]}, for the wonderful ${occasion} gift!`;
+        newMessage.push(message);
+    }
+    return newMessage;
+}
+writeCards(persons, surprise);
+
+function countDown(number){
+ while(number>= 0){
+    console.log(number);
+    number-- ;
+ }
+}
+countDown(10);
+
